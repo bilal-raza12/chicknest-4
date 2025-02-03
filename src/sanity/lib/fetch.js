9b68798cdb2d9b541cfdc7248343447d1c91ce97
bilal-raza12 <160725888+bilal-raza12@users.noreply.github.com>
2025-02-03 19:14:45 +0500

    Milestone 3

--- src/sanity/lib/fetch.js
+++ src/sanity/lib/fetch.js
@@ -10,7 +10,7 @@ export const client = createClient({
     
   });
 
-  export async function SanityFetch({query,params = {}}:{query:string,params?:{}}){
+  export async function SanityFetch({query,params = {}}){
 
       return await client.fetch(query,params) 
   }
